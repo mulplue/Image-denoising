ffa670af57f4d45da26678b97f3187612af4fd30
Hejun Wang <hejun_wang@zju.edu.cn>
2023-07-07 01:18:45 +0800

    slightly modify

--- data/dataloader.py
+++ data/dataloader.py
@@ -15,35 +15,10 @@ import torchvision.transforms as transforms
 np.set_printoptions(suppress=True, precision=4, linewidth=65535)
 import matplotlib.pyplot as plt
 
-# import sys
-# sys.path.append('.\\..\\')
+import sys
+sys.path.append(r'E:\Study\Technology\AI\github\Image-denoising')
 # print(sys.path)
-# from utils.add_noise import noise_mask_image # 不知道为啥报错
-
-def noise_mask_image(img, noise_ratio=[0.8,0.4,0.6]):
-    """
-    根据题目要求生成受损图片
-    :param img: cv2 读取图片,而且通道数顺序为 RGB
-    :param noise_ratio: 噪声比率，类型是 List,，内容:[r 上的噪声比率,g 上的噪声比率,b 上的噪声比率]
-                        默认值分别是 [0.8,0.4,0.6]
-    :return: noise_img 受损图片, 图像矩阵值 0-1 之间，数据类型为 np.array,
-             数据类型对象 (dtype): np.double, 图像形状:(height,width,channel),通道(channel) 顺序为RGB
-    """
-    # 受损图片初始化
-    noise_img = None
-    # -------------实现受损图像答题区域-----------------
-    noise_img = np.zeros(img.shape, np.double)
-    for k in range(3):
-        for i in range(img.shape[0]):
-            for j in range(img.shape[1]):
-                if random.random() < noise_ratio[k]:  # 如果生成的随机数小于噪声比例则将该像素点添加黑点
-                    noise_img[i][j][k] = 0  # 1 / 0
-                else:
-                    noise_img[i][j][k] = img[i][j][k]
-    
-    # -----------------------------------------------
-
-    return noise_img
+from utils.add_noise import noise_mask_image
 
 def get_filelist(path):
     files = glob.glob(path + '/*.png')
@@ -51,7 +26,7 @@ def get_filelist(path):
     file_names = []
     for file in files:
         # print(file)
-        file_name = file.split('/')[-1][-8:-4]
+        file_name = file.split('\\')[-1][:-4]
         cv2.waitKey(0)
 
         file_names.append(file_name)
@@ -60,7 +35,7 @@ def get_filelist(path):
 
 
 class ImageDataset(Dataset):
-    def __init__(self, dataset_path, eval_mode=False, size=(128, 256)):
+    def __init__(self, dataset_path, eval_mode=False, size=(256, 512)):
         self.eval_mode = eval_mode
         self.size = size
 
@@ -81,7 +56,7 @@ class ImageDataset(Dataset):
         else:
             filename = self.filenames[index]
 
-        path = self.dataset_path+'/'+filename+'.png'
+        path = self.dataset_path+'\\'+filename+'.png'
         raw_image = cv2.imread(path)
 
         # mirror the inputs for data augmentation
@@ -109,7 +84,7 @@ class ImageDataset(Dataset):
         return len(self.filenames)
     
 if __name__ == '__main__':
-    test_loader = DataLoader(ImageDataset(dataset_path='E:/Study/Technology/AI/github/Image-denoising/dataset/CBSD68/original_png',eval_mode=True), batch_size=1, shuffle=False, num_workers=1, pin_memory=True,persistent_workers=True)
+    test_loader = DataLoader(ImageDataset(dataset_path=r'.\dataset\CBSD68\original_png',eval_mode=True), batch_size=1, shuffle=False, num_workers=1, pin_memory=True,persistent_workers=True)
     # 'E:/Study/Technology/AI/github/Image-denoising/dataset/CBSD68/original_png'
 
     for i, batch in enumerate(test_loader):
