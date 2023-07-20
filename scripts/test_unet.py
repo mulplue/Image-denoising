f6d0a42c591d2cc3f985e419fde618725237a802
Hejun Wang <hejun_wang@zju.edu.cn>
2023-07-21 01:00:24 +0800

    restore of hejun

--- scripts/test_unet.py
+++ scripts/test_unet.py
@@ -1,7 +1,8 @@
 import copy
 import torch
-
+import os
 import sys
+import argparse
 # from os.path import join, dirname
 sys.path.append(r'/home/wanghejun/Desktop/wanghejun/Image-Denoising/github/Image-denoising/')
 # print(sys.path)
@@ -19,15 +20,27 @@ import glob
 import numpy as np
 import cv2
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='F1')
+args = parser.parse_args()
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 model = ResRemover().to(device)
-model.load_state_dict(torch.load("./trained_model/F1.pth",map_location=torch.device("cpu")))
+model.load_state_dict(torch.load("./trained_model/"+args.model+".pth"))
 
 
 
 def restore_image(noise_img, size=4):
     img = np.copy(noise_img)
+    # print("x")
+    # print(img)
+    # print(img.shape)
+
+
+    # cv2.imshow(img)
+
 
     img = torch.from_numpy(img).permute(2,0,1).to(device)
     img = img.unsqueeze(0)
@@ -40,10 +53,17 @@ def restore_image(noise_img, size=4):
     img = img.permute(1,2,0)
 
     img = img.cpu().detach().numpy()
+    # img = img/np.max(img)
     img = np.maximum(img,0)
     img = np.minimum(img,1)
+    # print("y")
+    # print(img)
+    # cv2.imshow(img)
+
+    # cv2.waitkey(0)
 
     img = cv2.resize(img,dsize=(noise_img.shape[1],noise_img.shape[0]))
+    # print(img.shape)
 
     # ---------------------------------------------------------------
 
@@ -65,7 +85,7 @@ def get_filelist(path):
 
 def read_img(path):
     img = Image.open(path)
-    img = img.resize((150,150))
+    # img = img.resize((150,150))
     img = np.asarray(img, dtype="uint8")
     # 获取图片数据类型对象的最大值和最小值
     info = np.iinfo(img.dtype)
@@ -80,6 +100,13 @@ for file in files:
     source = read_img("./dataset/test/input/"+file+".png")
     # target = read_image("./dataset/test/target/"+file+".png")
     result = restore_image(source)
+
+    # cv2.imshow("source",source)
+    # cv2.imshow("result",result)
+    # cv2.waitKey(0)
+
+
     cv2.cvtColor(result, cv2.COLOR_RGB2BGR, result)
-    cv2.imwrite("./dataset/test/restore_hejun/"+file+".png", result)
+    os.makedirs("./dataset/test/restore_hejun"+args.model, exist_ok=True)
+    cv2.imwrite("./dataset/test/restore_hejun"+args.model+"/"+file+".png", result*255)
 
