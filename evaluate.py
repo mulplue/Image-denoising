0816d3da7a5f13c9bb35eec394ed565c5e02a3ce
Jiahe Chen <mulplue@outlook.com>
2023-07-20 17:21:06 +0800

    add csv output

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import pandas as pd
 
 from utils.evaluate import *
 from utils.img import get_imgs
@@ -10,6 +11,7 @@ if __name__ == "__main__":
     parser.add_argument('--input_path', type=str, default='./dataset/test/input/')
     parser.add_argument('--restore_path', type=str, default='./dataset/test/restore_jiahe/')
     parser.add_argument('--target_path', type=str, default='./dataset/test/target/')
+    parser.add_argument('--author', type=str, default='jiahe')
     args = parser.parse_args()
     
     """Read images"""
@@ -18,10 +20,12 @@ if __name__ == "__main__":
     img_names, target_imgs = get_imgs(args.target_path)
 
     """"""
+    meric_index = ["l2_noise", "l2_restore", "ssim_noise", "ssim_restore", "csim_noise", "csim_restore"]
+    df = pd.DataFrame(columns=meric_index, index=img_names)
     l2_metrics = []
     ssim_metrics = []
     csim_metrics = []
-    for (img_name, noise_img, restore_img, target_img) in zip(img_names, noise_imgs, restore_imgs, target_imgs):
+    for i, (img_name, noise_img, restore_img, target_img) in enumerate(zip(img_names, noise_imgs, restore_imgs, target_imgs)):
         l2_noise = calc_l2(target_img, noise_img)
         l2_restore = calc_l2(target_img, restore_img)
         
@@ -31,4 +35,9 @@ if __name__ == "__main__":
         csim_noise = calc_csim(target_img, noise_img)
         csim_restore = calc_csim(target_img, restore_img)
         
+        all_merics = [l2_noise, l2_restore, ssim_noise, ssim_restore, csim_noise, csim_restore]
+        df.iloc[i] = all_merics
+        
+        output_path = "results/" + args.author + ".csv"
+        df.to_csv(output_path)
         
